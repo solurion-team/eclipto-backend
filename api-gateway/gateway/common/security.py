d03e918e9c1fa3d23244f8a792aa14b0e0e1ce29
evkost <mrtkostoev@gmail.com>
2024-05-13 12:05:34 +0300

    updated security

--- api-gateway/gateway/common/security.py
+++ api-gateway/gateway/common/security.py
@@ -4,11 +4,11 @@ from starlette.requests import Request
 from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
 
 
-class MockHTTPBearer(HTTPBearer):
+class GatewayHTTPBearer(HTTPBearer):
     mock_credentials = HTTPAuthorizationCredentials(scheme="bearer", credentials="mock")
 
     async def __call__(self, request: Request) -> Optional[HTTPAuthorizationCredentials]:
         return self.mock_credentials
 
 
-bearer_auth_scheme = MockHTTPBearer()
+bearer_auth_scheme = GatewayHTTPBearer()
