2ada5d750bfa762a84d2a211d3d659cbca4bb213
evkost <mrtkostoev@gmail.com>
2024-05-27 01:28:01 +0300

    improvement

--- api-gateway/gateway/task/models.py
+++ api-gateway/gateway/task/models.py
@@ -39,7 +39,7 @@ class UpdateTaskRequest(BaseModel):
         default=None, description="Description of the task",
         example="example: Go to metanit and learn all the chapters about Java."
     )
-    index: int = Field(
+    index: int | None = Field(
         description="Position index of the task", example=1
     )
     status_id: int | None = Field(
@@ -48,7 +48,7 @@ class UpdateTaskRequest(BaseModel):
     priority: Priority | None = Field(
         default=None, description="Priority of the task"
     )
-    due_date: str = Field(
+    due_date: str | None = Field(
         description="Due date"
     )
     assigned_user_id: int | None = Field(
