a508a89dda5289ee6025d41f4fd5929a6656b742
VyacheslavSamarin <slava.samarin.95@mail.ru>
2024-05-28 02:37:53 +0300

    changed name of functions

---
+++ api-gateway/gateway/gigachat/routes.py
@@ -0,0 +1,19 @@
+from fastapi import APIRouter
+from config import settings
+
+from langchain.schema import HumanMessage
+from langchain.chat_models.gigachat import GigaChat
+from langchain_core.prompts import load_prompt
+
+router = APIRouter()
+prompt_for_description_task = load_prompt(
+    "C:\\Users\slava\OneDrive\Desktop\project\eclipto-backend\\api-gateway\gateway\gigachat\config\\task_description_prompt.yaml")
+chat = GigaChat(credentials=settings.GIGACHAT_API_KEY, verify_ssl_certs=False)
+
+@router.get("/gigachat/test")
+async def gigachat_task_description(query: str):
+    try:
+        text = prompt_for_description_task.format(title=query)
+        return chat([HumanMessage(content=text)])
+    except Exception as e:
+        raise e
